14153dfe28b15e1cd16c34aa52f5f788f6cc6438
Jeremy Lloyd <jeremylloyd3@gmail.com>
2022-10-03 18:23:44 +1100

    Updated calculator styling and added functionality to adjust views and price based on the slider value

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
+views = [10000, 50000, 100000, 500000, 1000000];
+prices = [8, 12, 16, 24, 36];
+
 inputRange = document.querySelector(".slider__input");
-// debugger;
-inputRange.addEventListener("input", (event) => {
-  console.log(event);
-  // debugger;
-  const target = event.target;
-  target.style.backgroundImage = `
-  linear-gradient(
-    to right,
-    var(--theme-lightcyan) 0%,
-    var(--theme-lightcyan) ${target.valueAsNumber * 25}%,
-    var(--theme-lightgreyblue) ${target.valueAsNumber * 25}%,
-    var(--theme-lightgreyblue) 100%
-  )
-`;
-});
+inputRange.addEventListener("input", updateSlider);
+
+function updateSlider() {
+  option = inputRange.valueAsNumber;
+
+  updateViews(views[option]);
+  updatePrice(prices[option]);
+  updateSliderBarColour();
+}
+
+function updateViews(viewCount) {
+  const viewFormatter = Intl.NumberFormat("en", { notation: "compact" });
+  viewsStr = viewFormatter.format(viewCount);
+  document.querySelector(".calc__views").innerHTML = `${viewsStr} pageviews`;
+}
+
+function updatePrice(price) {
+  const priceFormatter = Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  });
+  priceStr = priceFormatter.format(price);
+  document.querySelector(".calc__price").innerHTML = priceStr;
+}
+
+function updateSliderBarColour() {
+  inputRange.style.backgroundImage = `
+    linear-gradient(
+      to right,
+      var(--theme-lightcyan) 0%,
+      var(--theme-lightcyan) ${inputRange.valueAsNumber * 25}%,
+      var(--theme-lightgreyblue) ${inputRange.valueAsNumber * 25}%,
+      var(--theme-lightgreyblue) 100%
+    )
+  `;
+}
